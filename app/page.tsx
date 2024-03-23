4423e53c08cbf93535e975e09671837f3e916e34
Kyou <kit115kat624@gmail.com>
2024-03-23 16:46:03 +0900

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
               priority
             />
           </a>
+
         </div>
       </div>
 
@@ -38,7 +40,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      <Link href="/posts" className="hover:underline underline-offset-4">Posts Page!</Link>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
